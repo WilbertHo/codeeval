5ad954a9e18e56c2f4ca17b0dc0e8c15e3f1b3a2
Wil Ho <wilho@wetafx.co.nz>
2014-12-01 15:21:44 +1300

    [prime palindrome] (java) initial.

---
+++ prime_palindrome/java/src/test/java/PrimePalindromeTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class PrimePalindromeTest {
+    @Test
+    public void testOutput() {
+        PrimePalindrome pp = new PrimePalindrome();
+        // assertEquals("onetwo", result);
+    }
+}
