1fd2dc80bd69e15eb1b3af80440b808ebb40cc13
Wilbert Ho <wilbert.ho@gmail.com>
2015-01-12 18:20:47 +1300

    [sum of primes] (Python) Document Sieve algorithm. Rework loop to use more idiomatic Python.

--- sum_of_primes/py/SieveOfEratosthenes.py
+++ sum_of_primes/py/SieveOfEratosthenes.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python
 
 
-class SieveOfEratosthenes(object):
-    def __init__(self):
-        pass
-
 def sieve_of_eratosthenes(n):
-    # generate list of ints from 2 to n
+    # Generate a list of possible primes, with 1 denoting prime and 0
+    # denoting not-prime
     nums = [1 for i in xrange(0, n + 1)]
 
+    # Start at the first prime number, p = 2, and set all multiples
+    # of p to not-prime
+    # Find the next number greater than p that hasn't been marked
+    # not-prime and repeat
     for i in xrange(2, n + 1):
         if nums[i] == 0:
             continue
-        j = i
-        while(j < n + 1):
-            j += i
-            if j < len(nums):
-                nums[j] = 0
+        # for j in xrange(i << 1, n + 1, i):
+        for j in xrange(i * 2, n + 1, i):
+            nums[j] = 0
 
+    # 0 and 1 are not prime numbers, return any marked as prime
     return [i for i in xrange(0, len(nums)) if nums[i] == 1][2:]
 
 
