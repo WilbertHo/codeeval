174d69d9b0242024ec946286acd04b8f8ba89b17
Wilbert Ho <wilbert.ho@gmail.com>
2015-01-30 12:08:57 +1300

    [bay bridges] (python) Fix function parameters: should be 'segments' instead of 'coords'. Add docstrings.

--- hard/bay_bridges/py/baybridges.py
+++ hard/bay_bridges/py/baybridges.py
@@ -3,7 +3,7 @@ import fileinput
 import re
 from collections import namedtuple
 from decimal import Decimal, getcontext
-from itertools import permutations
+from itertools import combinations
 
 
 Coord = namedtuple('Coord', ['x', 'y'])
@@ -22,16 +22,23 @@ def parse_input(input):
 
 # formulas are from
 # https://stackoverflow.com/questions/328107/how-can-you-determine-a-point-is-between-two-other-points-on-a-line-segment
-def is_between(line, coord):
-    cross = (coord.y - line.head.y) * (line.tail.x - line.head.x) - (coord.x - line.head.x) * (line.tail.y - line.head.y)
+def is_between(segment, coord):
+    """ Determine if coordinate (x, y) lies on line l.
+        Calculates if coord (x, y) lies between the coords comprising
+        line segment l.
+        params:
+            line: Segment
+            coord: Coord
+    """
+    cross = (coord.y - segment.head.y) * (segment.tail.x - segment.head.x) - (coord.x - segment.head.x) * (segment.tail.y - segment.head.y)
     if abs(cross.quantize(Decimal('.000001'))) != 0:
         return False
 
-    dot = (coord.x - line.head.x) * (line.tail.x - line.head.x) + (coord.y - line.head.y) * (line.tail.y - line.head.y)
+    dot = (coord.x - segment.head.x) * (segment.tail.x - segment.head.x) + (coord.y - segment.head.y) * (segment.tail.y - segment.head.y)
     if dot < 0:
         return False
 
-    length = (line.tail.x - line.head.x) * (line.tail.x - line.head.x) + (line.tail.y - line.head.y) * (line.tail.y - line.head.y)
+    length = (segment.tail.x - segment.head.x) * (segment.tail.x - segment.head.x) + (segment.tail.y - segment.head.y) * (segment.tail.y - segment.head.y)
     if dot > length:
         return False
 
@@ -41,6 +48,13 @@ def is_between(line, coord):
 # intersection formulas are from
 # https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection#Given_two_points_on_each_line
 def intersection(s1, s2):
+    """ Calculate the point of intersection between segments s1 and s2.
+        Extend line segments s1 and 2 to infinity and calculate the
+        point of intersection.
+        params:
+            s1: Segment
+            s2: Segment
+    """
     x = ((s1.head.x * s1.tail.y - s1.head.y * s1.tail.x) * (s2.head.x - s2.tail.x) - (s1.head.x - s1.tail.x) * (s2.head.x * s2.tail.y - s2.head.y * s2.tail.x)) / \
         ((s1.head.x - s1.tail.x) * (s2.head.y - s2.tail.y) - (s1.head.y - s1.tail.y) * (s2.head.x - s2.tail.x))
 
@@ -50,23 +64,32 @@ def intersection(s1, s2):
     return x, y
 
 
-def intersects(coords_a, coords_b):
-    return all([is_between(coords_a, Coord(*intersection(coords_a, coords_b))),
-                is_between(coords_b, Coord(*intersection(coords_a, coords_b)))])
+def intersects(segment_a, segment_b):
+    """
+    """
+    return all([is_between(segment_a, Coord(*intersection(segment_a, segment_b))),
+                is_between(segment_b, Coord(*intersection(segment_a, segment_b)))])
 
 
 def main():
     input = [line.strip() for line in fileinput.input()]
     bridges = dict(map(parse_input, input))
 
-    crossers = set([sorted((bridge_a, bridge_b))[0]
-                    for (bridge_a, coords_a), (bridge_b, coords_b)
-                    in permutations(bridges.iteritems(), 2)
-                    if intersects(coords_a, coords_b)])
+    # crossers = [sorted((bridge_a, bridge_b))
+    #             for (bridge_a, coords_a), (bridge_b, coords_b)
+    #             in combinations(bridges.iteritems(), 2)
+    #             if intersects(coords_a, coords_b)]
 
-    for bridge in sorted(bridges.keys()):
-        if bridge not in crossers:
-            print bridge
+    # for bridge in sorted(bridges.keys()):
+    #     if bridge not in crossers:
+    #         print bridge
+
+    for size in range(len(bridges), 0, -1):
+        for subset in combinations(bridges.iteritems(), size):
+            for (bridge_a, segment_a), (bridge_b, segment_b) \
+                in combinations(subset, 2):
+                    if not intersects(segment_a, segment_b):
+                        print bridge_a, bridge_b
 
 
 if __name__ == '__main__':
