163cf7d401ae4e83faf2d67f767b893c1a243458
Wilbert Ho <wilbert.ho@gmail.com>
2015-02-02 11:26:30 +1300

    [bay bridges] (python) Fixed test cases.

--- hard/bay_bridges/py/tests/test_baybridges.py
+++ hard/bay_bridges/py/tests/test_baybridges.py
@@ -4,11 +4,11 @@ from baybridges import Point, Segment, intersects, get_nonintersecting
 
 def test_intersects():
     s1 = Segment(Point(37.532599, -122.218094), Point(37.615863, -122.097244))
-    s2 = Segment(Point(37.516262, -122.198181), Point(37.653383, -122.151489))
-    s3 = Segment(Point(37.788353, -122.387695), Point(37.829853, -122.294312))
+    s2 = Segment(Point(37.788353, -122.387695), Point(37.829853, -122.294312))
+    s3 = Segment(Point(37.516262, -122.198181), Point(37.653383, -122.151489))
 
-    eq_(intersects(s1, s2), True)
-    eq_(intersects(s1, s3), False)
+    eq_(intersects(s1, s2), False)
+    eq_(intersects(s1, s3), True)
 
 
 def test_get_nonintersecting():
@@ -16,4 +16,4 @@ def test_get_nonintersecting():
             2: Segment(Point(37.788353, -122.387695), Point(37.829853, -122.294312)),
             3: Segment(Point(37.516262, -122.198181), Point(37.653383, -122.151489))}
 
-    eq_(get_nonintersecting(bridges), [1, 2])
+    eq_(get_nonintersecting(bridges), [2, 3])
