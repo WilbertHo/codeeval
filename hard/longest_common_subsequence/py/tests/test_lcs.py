13bfe3eec2a8e519e24e7ca1873759925086a111
Wilbert Ho <wilbert.ho@gmail.com>
2015-02-04 22:15:13 +1300

    [longest common subsequence] (python) Tests.

--- hard/longest_common_subsequence/py/tests/test_lcs.py
+++ hard/longest_common_subsequence/py/tests/test_lcs.py
@@ -1,6 +1,7 @@
 from nose.tools import eq_
 import lcs
 
-def test_lcs():
-    eq_(lcs.lcs['XMJYAUZ', 'MZJAWXU'], 'MJAU')
-    eq_(lcs.lcs['BANANA', 'ATANA'], 'AANA')
+def test_longest_common_subsequence():
+    eq_(lcs.longest_common_subsequence('XMJYAUZ', 'MZJAWXU'), 'MJAU')
+    eq_(lcs.longest_common_subsequence('BANANA', 'ATANA'), 'AANA')
+    eq_(lcs.longest_common_subsequence('ABDCDEF', 'AZD'), 'AD')
