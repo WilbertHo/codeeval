178551fb3b9d39255b57be8302026b5ab94ee65c
Wilbert Ho <wilbert.ho@gmail.com>
2015-02-06 00:29:44 +1300

    [bay bridges] (scala) Add test skeleton.

---
+++ hard/bay_bridges/scala/src/test/scala/baybridges_test.scala
@@ -0,0 +1,10 @@
+import org.scalatest._
+
+class BayBridgesSpecs extends FlatSpec with Matchers {
+  it should "detect no matches" in {
+    pending
+    // val detector = new BayBridges()
+    // val anagrams = detector.matches(Seq("hello", "world", "zombies", "pants"))
+    // anagrams should equal (Seq())
+  }
+}
