a6b10aaaf482c174636bf3e2b2b8173223eccfdf
Wilbert Ho <wilbert.ho@gmail.com>
2015-02-06 00:29:23 +1300

    [bay bridges] (scala) Add Coords and Segment classes. Clean up parse_input function.

--- hard/bay_bridges/scala/src/main/scala/baybridge.scala
+++ hard/bay_bridges/scala/src/main/scala/baybridge.scala
@@ -1,15 +1,38 @@
+class Coords(val x: Double, val y: Double) {}
+class Segment(val head: Coords, val tail: Coords) {}
+
 class BayBridges {
 }
 
 object Main {
-  def parse_input(input: Traversable[String]): Map[Int,Array[Double]] = {
-    return input.map(_.split(""":\s*""")).map(x=>(x(0).toInt, x(1).split(""",\s*""").map(_.replaceAll("""[^0-9-.]""", "")).map(_.toDouble))).toMap
+  def parse_input(input: Traversable[String]): Map[Int, Segment] = {
+    // ("1: ([37.788353, -122.387695], [37.829853, -122.294312])", ...
+    // split by ":\s*" becomes:
+    // (Array("1", "([37.788353, -122.387695], [37.829853, -122.294312])"))
+    return input.map(_.split(""":\s*""")).map {
+      case Array(bridge, coords) =>
+        (bridge.toInt,
+         // "([37.788353, -122.387695], [37.829853, -122.294312])"
+         // replace "[^0-9-.]" and convert to double:
+         // Array(37.788353, -122.387695, 37.829853, -122.294312)
+         coords.split(""",\s*""" ).map(
+           _.replaceAll("""[^0-9-.]""", "")).map(
+             // Take every 2 values and create a Coords object
+             _.toDouble).sliding(2, 2).map {
+               case Array(x, y) => new Coords(x, y)
+             // Take 2 Coords and create a Segment
+             }.sliding(2, 2).map {
+               case Seq(x, y) => new Segment(x, y)
+             // Take the first (and only) element in the iterator
+             }.next()
+        )
+      }.toMap
   }
 
   def main(args: Array[String]) {
     val input = (if (args.length < 1) io.Source.stdin
                  else io.Source.fromFile(args(0))).getLines().toVector
     
-    parse_input(input)
+    print(parse_input(input))
   }
 }
