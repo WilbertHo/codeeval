5131a7e62a30ef959c43b1524392c77cb9543ade
Wilbert Ho <wilbert.ho@gmail.com>
2015-02-06 18:21:46 +1300

    [bay bridges] (scala) WIP: Added intersection, is_between and intersects. Need function to return non-intersecting bridges.

--- hard/bay_bridges/scala/src/main/scala/baybridge.scala
+++ hard/bay_bridges/scala/src/main/scala/baybridge.scala
@@ -1,7 +1,54 @@
-class Coords(val x: Double, val y: Double) {}
-class Segment(val head: Coords, val tail: Coords) {}
+class Point(val x: Double, val y: Double) {}
+class Segment(val head: Point, val tail: Point) {}
 
 class BayBridges {
+  def intersection(s1: Segment, s2:Segment): Point = {
+    /* Calculate the point of intersection between segments s1 and s2.
+     * Extend line segments s1 and 2 to infinity and calculate the
+     * point of intersection.
+     */ 
+
+      val x = ((s1.head.x * s1.tail.y - s1.head.y * s1.tail.x) * (s2.head.x - s2.tail.x) - (s1.head.x - s1.tail.x) * (s2.head.x * s2.tail.y - s2.head.y * s2.tail.x)) /
+              ((s1.head.x - s1.tail.x) * (s2.head.y - s2.tail.y) - (s1.head.y - s1.tail.y) * (s2.head.x - s2.tail.x))
+
+      val y = ((s1.head.x * s1.tail.y - s1.head.y * s1.tail.x) * (s2.head.y - s2.tail.y) - (s1.head.y - s1.tail.y) * (s2.head.x * s2.tail.y - s2.head.y * s2.tail.x)) /
+              ((s1.head.x - s1.tail.x) * (s2.head.y - s2.tail.y) - (s1.head.y - s1.tail.y) * (s2.head.x - s2.tail.x))
+
+      return new Point(x, y)
+  }
+
+  def is_between(segment: Segment, point: Point): Boolean = {
+    /* Determine if point (x, y) lies on line l.
+     * Calculates if point (x, y) lies between the points comprising
+     * line segment l.
+     */
+
+    val cross = (point.y - segment.head.y) * (segment.tail.x - segment.head.x) - (point.x - segment.head.x) * (segment.tail.y - segment.head.y)
+    if (math.abs(math.round(cross)) != 0)
+        return false
+
+    val dot = (point.x - segment.head.x) * (segment.tail.x - segment.head.x) + (point.y - segment.head.y) * (segment.tail.y - segment.head.y)
+    if (dot < 0)
+        return false
+
+    val length = (segment.tail.x - segment.head.x) * (segment.tail.x - segment.head.x) + (segment.tail.y - segment.head.y) * (segment.tail.y - segment.head.y)
+    if (dot > length)
+        return false
+
+    return true
+  }
+
+  def intersects(segment_a: Segment, segment_b: Segment): Boolean = {
+    /* Check if line segment_a intersects line segment_b
+     * Extend segment_a and segment_b to infinity and calculate the
+     * point p of intersection, then determine if point p lies between
+     * both segment_a and segment_b.
+     */
+      
+    val foo = List(is_between(segment_a, intersection(segment_a, segment_b)),
+                   is_between(segment_b, intersection(segment_a, segment_b)))
+    return foo.forall(_ == true)
+  }
 }
 
 object Main {
@@ -17,10 +64,10 @@ object Main {
          // Array(37.788353, -122.387695, 37.829853, -122.294312)
          coords.split(""",\s*""" ).map(
            _.replaceAll("""[^0-9-.]""", "")).map(
-             // Take every 2 values and create a Coords object
+             // Take every 2 values and create a Point object
              _.toDouble).sliding(2, 2).map {
-               case Array(x, y) => new Coords(x, y)
-             // Take 2 Coords and create a Segment
+               case Array(x, y) => new Point(x, y)
+             // Take 2 Point and create a Segment
              }.sliding(2, 2).map {
                case Seq(x, y) => new Segment(x, y)
              // Take the first (and only) element in the iterator
--- hard/bay_bridges/scala/src/test/scala/baybridges_test.scala
+++ hard/bay_bridges/scala/src/test/scala/baybridges_test.scala
@@ -1,10 +1,33 @@
 import org.scalatest._
 
 class BayBridgesSpecs extends FlatSpec with Matchers {
-  it should "detect no matches" in {
-    pending
-    // val detector = new BayBridges()
-    // val anagrams = detector.matches(Seq("hello", "world", "zombies", "pants"))
-    // anagrams should equal (Seq())
+  it should "is between" in {
+    val bb = new BayBridges()
+
+    val head = new Point(37.788353, -122.387695)
+    val tail = new Point(37.829853, -122.294312)
+    val not_in_between = new Point(37.474858, -122.131577)
+    val segment = new Segment(head, tail)
+
+    bb.is_between(segment, head) should equal (true)
+    bb.is_between(segment, not_in_between) should equal (false)
+  }
+
+  it should "intersects" in {
+    val bb = new BayBridges()
+
+    val s1 = new Segment(new Point(37.532599, -122.218094),
+                         new Point(37.615863, -122.097244))
+    val s2 = new Segment(new Point(37.516262, -122.198181),
+                         new Point(37.653383, -122.151489))
+
+    val s3 = new Segment(new Point(37.788353, -122.387695),
+                         new Point(37.829853, -122.294312))
+
+    val s4 = new Segment(new Point(37.504824, -122.181702),
+                         new Point(37.633266,-122.121964))
+
+    bb.intersects(s1, s2) should equal (true)
+    bb.intersects(s3, s4) should equal (false)
   }
 }
