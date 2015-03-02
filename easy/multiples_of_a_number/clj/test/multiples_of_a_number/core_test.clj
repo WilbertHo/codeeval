07fdcfcd9523784f19dea454352bfab450bcd9cf
Wilbert Ho <wilbert.ho@gmail.com>
2015-03-02 23:01:03 +1300

    [multiples of a number] (clj) Initial.

---
+++ easy/multiples_of_a_number/clj/test/multiples_of_a_number/core_test.clj
@@ -0,0 +1,7 @@
+(ns multiples-of-a-number.core-test
+  (:require [clojure.test :refer :all]
+            [multiples-of-a-number.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
